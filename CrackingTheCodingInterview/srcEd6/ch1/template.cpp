f8c977a1366994e49f7920655e7b07de93d4f895
Ray <raymonwhite42+gitlab@gmail.com>
2018-03-11 20:58:21 +0000

    Doing Ch1, Q3, URLify

---
+++ CrackingTheCodingInterview/srcEd6/ch1/template.cpp
@@ -0,0 +1,44 @@
+/*
+ * =========================================================================
+ * Copyright (c) 2018, Raymon White - All Rights Reserved
+ * Author:  Raymon White (ray), rayaxiom42 AT gmail DOT com
+ * =========================================================================
+ */
+
+// This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN  
+#include "./../catch.hpp"
+
+#include<iostream>
+using std::cout; using std::endl;
+
+// This is used as my own debug output. I need to set RAYDEBUG=false/true
+#define rcout if(RAYDEBUG) cout 
+static bool RAYDEBUG=false;
+#define RDEBUG(x) cout << '>' << #x << ':' << x << '\n';
+
+
+// #include here
+
+
+// Question here.
+
+
+// Implementation here
+
+
+
+
+TEST_CASE("Testing ", "[]" )
+{
+  SECTION("Sec Testing ")
+  {
+    REQUIRE(func("","")==true);
+  }
+}
+
+//auto main()->int
+//{
+//  return 0;
+//}
+
