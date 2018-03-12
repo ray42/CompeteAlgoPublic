85394391c913319816cf987ea2a637953d0c104a
Ray <raymonwhite42+gitlab@gmail.com>
2018-03-12 16:57:03 +0000

    Added/modified template.cpp

--- CrackingTheCodingInterview/srcEd6/ch1/rrr5_OneAway.cpp
+++ CrackingTheCodingInterview/srcEd6/ch1/rrr5_OneAway.cpp
@@ -17,11 +17,37 @@ using std::cout; using std::endl;
 static bool RAYDEBUG=false;
 #define RDEBUG(x) cout << '>' << #x << ':' << x << '\n';
 
+// Question here.
+// 1.5 One Away: There are three types of edits that can be performed on 
+// strings: 
+// 1) insert a character,
+// 2) remove a character, or
+// 3) replace a character.
+//
+//Given two strings, write a function to check if they are one edit (or zero
+//edits) away.
+//
+//EXAMPLE
+//pale,  ple   -> true
+//pales, pale  -> true
+//pale,  bale  -> true
+//pale,  bake  -> false
+//
+//Hints:
+// #23: Start with the easy thing. Can you check each of the conditions
+//  separately?
+// #97: What is the relationship between the ``insert character'' option
+//  and the ``remove character'' option? Do these need to be two separate
+//  checks?
+// #130: Can you do all three checks in a single pass?
+//
+//The only way I know how to check this is via dynamic programming:
+//https://www.geeksforgeeks.org/dynamic-programming-set-5-edit-distance
+
 
 // #include here
 
 
-// Question here.
 
 
 // Implementation here
